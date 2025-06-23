17313c6d9350a9c31da973b188e819b67ac2643f
Harry Newman <h.newman2005@outlook.com>
2025-06-23 23:14:53 +0100

    formatting

--- compress.c
+++ compress.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include <string.h>
+#include <stdio.h>
 
 bool argument_check(int argc, char* argv[]);
 
@@ -14,20 +15,18 @@ int main(int argc, char* argv[])
 
 bool argument_check(int argc, char* argv[])
 {
-    // Usage: ./boots -c (-rle|-huff) <input_file> <output_file>  OR  ./boots -d <input_file> <output_file>
-    if (argc != 5)
-    {
+    if (argc != 5) {
+        printf("Usage: ./boots (-c|-d) (-rle|-huff) <input_file> <output_file>");
         return false;
     }
 
-    if (strcmp(argv[1], "-c") != 0 && strcmp(argv[1], "-d") != 0)
-    {
+    if (strcmp(argv[1], "-c") != 0 && strcmp(argv[1], "-d") != 0) {
         return false;
     }
 
-    if (strcmp(argv[2], "-rle") != 0 && strcmp(argv[2], "-huff") != 0)
-    {
+    if (strcmp(argv[2], "-rle") != 0 && strcmp(argv[2], "-huff") != 0) {
         return false;
     }
+
     return true;
 }
