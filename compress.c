1f5cccc44ee1b7fd9c37e622b8a0945406848c26
Harry Newman <h.newman2005@outlook.com>
2025-06-23 23:32:16 +0100

    file i/o check + sample.txt

--- compress.c
+++ compress.c
@@ -20,13 +20,25 @@ bool argument_check(int argc, char* argv[])
         return false;
     }
 
-    if (strcmp(argv[1], "-c") != 0 && strcmp(argv[1], "-d") != 0) {
+    if (strcmp(argv[1], "-c") != 0 && strcmp(argv[1], "-d") != 0) 
+    {
+        printf("First argument must be -c or -d.\n");
         return false;
     }
 
-    if (strcmp(argv[2], "-rle") != 0 && strcmp(argv[2], "-huff") != 0) {
+    if (strcmp(argv[2], "-rle") != 0 && strcmp(argv[2], "-huff") != 0)
+    {
+        printf("Second argument must be -rle or -huff.\n");
         return false;
     }
+
+    FILE *file = fopen(argv[3], "rb");
+    if (file == NULL)
+    {
+        printf("Could not open input file: %s", argv[3]);
+        return false;
+    }
+    fclose(file);
 
     return true;
 }
