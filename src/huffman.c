03215136978552441064d23233341a341c13cfcc
Harry Newman <h.newman2005@outlook.com>
2025-07-08 10:26:30 +0100

    Added frequencyTable

--- src/huffman.c
+++ src/huffman.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef unsigned char byte;
 typedef struct TreeNode {
@@ -7,4 +8,11 @@ typedef struct TreeNode {
 } TreeNode;
 
 void huffman_compress(FILE *input_file, int input_length,
-                      const char *output_filename);
+                      const char *output_filename) {
+  byte *frequencyTable = malloc(sizeof(byte) * 256);
+  if (frequencyTable == NULL) {
+    printf("Memory allocation failed: frequencyTable");
+    return;
+  }
+  return;
+}
